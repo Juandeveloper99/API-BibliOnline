5e2b1a4b80eac90232bc794a00392ff2d75b6f63
Juan Calix <168151925+Juandeveloper99@users.noreply.github.com>
2024-08-06 20:43:19 -0600

    login, orden de las tablas por carpetas y protegiendo rutas

--- config.js
+++ config.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2'); //usar la libreria4
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 //configurar la base de datos
 const config = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '6012',
-    database: 'Biblioteca_app',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS ,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
 
 module.exports = config; // exportar la config
